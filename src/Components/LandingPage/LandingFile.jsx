f67b607ab2ae6b210e285293e9df294c2d835eb3
Yash Bavishi <bavishiys63@gmail.com>
2023-12-05 11:53:31 -0800

    frontend

--- src/Components/LandingPage/LandingFile.jsx
+++ src/Components/LandingPage/LandingFile.jsx
@@ -1,24 +1,50 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import './LandingFile.css'
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 const LandingFile = () => {
+    const [result, setResult] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate();
     const [file, setFile] = useState()
     const upload = () => {
         const formData = new FormData();
         formData.append('file', file)
         axios.post('http://localhost:8000/upload', formData )
-        .then( res => {})
+        .then( async (res) => {setResult(await res.data); console.log(await res.data); setIsLoading(false)})
         .catch(er => console.log(er));
     }
+
+    // useEffect(() => {
+    //   async function validator()  {
+
+    //     const token = localStorage.getItem("token")
+    //     const data = {"token": token}
+    //     const res = await fetch("http://localhost:8000/api/validateJWT", {method: "POST", mode: "cors", headers:{"Content-Type": "application/json"},body: JSON.stringify(data)})
+    //     if (await res.status === 200) {
+    //       console.log("HOLA")
+    //     } else if (res.status === 400) {
+    //       navigate('/')
+    //       console.log("SOMETHING")
+    //     }
+    //   }
+    //   validator()
+    // }, [])
+
+
   return (
      <div className="container">
        <div className="heading">
           <h3>Welcome to Early Onset Alzheimer's Detector</h3>
        </div>
+       <div className='result'>
+        {isLoading ? <p>Results are getting loaded</p>: <p>{parseFloat(result.replace('[[', '').replace(']]','').split(' ')[0])*100 + '% Chance of Alzheimer'
+}</p>}
+       </div>
        <div className="uploads">
         <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-        <button type="button" class="button" onClick={upload}>upload</button> 
+        <button type="button" className="button" onClick={upload}>upload</button> 
       </div>
      </div>
   )
--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -4,10 +4,52 @@ import './LoginSignup.css'
 import user_icon from '../Assets/person.png';
 import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
-
+import {useNavigate} from 'react-router-dom';
 
 const LoginSignup = () => {
+ const navigate = useNavigate();
  const [action, setAction] = useState("Login")
+ const [name, setName] = useState("")
+ const [email, setEmail] = useState("")
+ const [password, setPassword] = useState("")
+
+ const signup = async () => {
+    const data = {"name": name, "email": email, "password": password}
+    console.log(data)
+    console.log(JSON.stringify(data))
+    try {
+      const res = await fetch("http://localhost:8000/api/signup", {method: "POST", mode: "cors", headers:{"Content-Type": "application/json"},body: JSON.stringify(data)})
+      if (await res.status === 200) {
+        console.log("User Created")
+        setAction("Login")
+
+      } else if (await res.status === 422) { 
+        console.log("Anyone of the field is missing or name is to short")
+      } else if (await res.status === 400){
+        console.log("Already Exists!!")
+      }
+    } catch (e) {
+    }
+ }
+
+ const validation = async () => {
+    const data = {"email": email, "password": password}
+    console.log(data)
+    console.log(JSON.stringify(data))
+    try {
+      const res = await fetch("http://localhost:8000/api/signin", {method: "POST", mode: "cors", headers:{"Content-Type": "application/json"},body: JSON.stringify(data)})
+      if (await res.status === 200) {
+        const token = await res.json()
+        localStorage.setItem("token", token.token)
+        navigate('/landingpage')
+      } else if (await res.status === 400) { 
+        console.log("Email doesnt exist ")
+      } else if (await res.status === 401){
+        console.log("Password doesnt match ")
+      }
+    } catch (e) {
+    }
+ }
   return (
     <div className="container">
       <div className="header">
@@ -17,22 +59,22 @@ const LoginSignup = () => {
       <div className="inputs">
         {action==="Login"?<div></div>: <div className="input">
             <img src={user_icon} alt="" />
-            <input type="text" placeholder='Name' />
+            <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)} />
         </div>}
         
         <div className="input">
             <img src={email_icon} alt="" />
-            <input type="email" placeholder='Email-ID' />
+            <input type="email" placeholder='Email-ID' onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className="input">
             <img src={password_icon} alt="" />
-            <input type="password" placeholder='Password' />
+            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
         </div>
       </div>
       {/* <div className="forgot-password">Lost Password? <span>Click Here</span></div> */}
       <div className="submit-container">
-      <div className={action==="Login"?"submit gray": "submit"} onClick={() => {setAction("Sign Up")}}>SignUp</div>
-      <div className={action==="Signup"?"submit gray": "submit"} onClick={() => {setAction("Login")}}>Login</div>
+      <div className={action==="Login"?"submit gray": "submit"} onClick={() => {setAction("Sign Up"); signup()}}>SignUp</div>
+      <div className={action==="Signup"?"submit gray": "submit"} onClick={() => {setAction("Login"); validation()}}>Login</div>
       </div>
     </div>
   )
